efc592e01edb3fb2c2e5a25f2f1386aa1e421f00
Colton0012 <coltond0012@gmail.com>
2020-11-16 21:12:37 -0800

    Added tests for both the palindrome and descending

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,145 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+TEST(PracticeTest, is_not_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome_symbols)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("!!!!z!!!!");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome_blank)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_not_simple_palindrome_close_words)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("teststet");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_descending)
+{
+    Practice obj;
+	int first = 1;
+	int second = 2;
+	int third = 3;
+	bool actual;
+    obj.sortDescending(first, second, third);
+	if (first >= second && second >= third)
+	{
+		actual = true;
+	}else
+	{
+		actual = false;
+	}
+	ASSERT_TRUE(actual);    
+}
+
+TEST(PracticeTest, is_descending_with_repeat)
+{
+    Practice obj;
+	int first = 1;
+	int second = 1;
+	int third = 2;
+	bool actual;
+    obj.sortDescending(first, second, third);
+	if (first >= second && second >= third)
+	{
+		actual = true;
+	}else
+	{
+		actual = false;
+	}
+	ASSERT_TRUE(actual);    
+}
+
+TEST(PracticeTest, is_descending_with_first_larger)
+{
+    Practice obj;
+	int first = 4;
+	int second = 1;
+	int third = 2;
+	bool actual;
+    obj.sortDescending(first, second, third);
+	if (first >= second && second >= third)
+	{
+		actual = true;
+	}else
+	{
+		actual = false;
+	}
+	ASSERT_TRUE(actual);    
+}
+
+TEST(PracticeTest, is_descending_with_second_larger)
+{
+    Practice obj;
+	int first = 4;
+	int second = 5;
+	int third = 2;
+	bool actual;
+    obj.sortDescending(first, second, third);
+	if (first >= second && second >= third)
+	{
+		actual = true;
+	}else
+	{
+		actual = false;
+	}
+	ASSERT_TRUE(actual);    
+}
+
+TEST(PracticeTest, is_descending_already)
+{
+    Practice obj;
+	int first = 100;
+	int second = 9;
+	int third = 1;
+	bool actual;
+    obj.sortDescending(first, second, third);
+	if (first >= second && second >= third)
+	{
+		actual = true;
+	}else
+	{
+		actual = false;
+	}
+	ASSERT_TRUE(actual);    
+}
+
+TEST(PracticeTest, is_descending_with_negative)
+{
+    Practice obj;
+	int first = -1;
+	int second = 0;
+	int third = -9;
+	bool actual;
+    obj.sortDescending(first, second, third);
+	if (first >= second && second >= third)
+	{
+		actual = true;
+	}else
+	{
+		actual = false;
+	}
+	ASSERT_TRUE(actual);    
 }
